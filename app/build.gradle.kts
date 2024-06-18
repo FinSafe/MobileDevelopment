ea858f5c233023bfccedef0fbc4aba16d7fc8827
Femmy Friscilla <2172030@maranatha.ac.id>
2024-06-18 13:20:30 +0700

    add register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.github.parniyan7:DotLoading:4.0.0")
 }
